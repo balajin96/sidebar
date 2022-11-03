ba55718688bd623ad8544b3177c6d19ccdf79d59
balaji <43362896+balajin96@users.noreply.github.com>
2022-11-03 15:35:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React, { Fragment } from "react";
 import Sidebar from "./Sidebar";
-// import Footer from "./Footer";
 function App() {
   return (
     <Fragment>
-      <Sidebar />
-      {/* <Footer /> */}
+      <Sidebar/>
     </Fragment>
   );
 }
